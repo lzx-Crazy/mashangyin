6f22e31263a4b8835d608129e5ec41f4b0ebc731
lzxCrazy <929230347@qq.com>
2018-09-15 15:01:56 +0800

    lzx

---
+++ src/pages/lzxbytransactionInformation/lzxbytransactionInformation.js
@@ -0,0 +1,12 @@
+const content = require('./lzxbytransactionInformation.ejs');
+const botSource = require('../../plugin/lzxjktransactionInformation.ejs');
+const layout = require('../../template/layout.js');
+const pf = {
+  pageTitle: '最新成交信息',
+  rtBtn3: 'nav-yiwen'
+}
+
+
+module.exports = layout.init({
+  pf
+}).run(content(),'',botSource());
